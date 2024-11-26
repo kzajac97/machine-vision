fb15d9903e717ee2bc2185a78591cc9251288b80
Krzysztof Zając <kzajac1997@gmail.com>
2024-11-26 19:54:42 +0100

    [classes/interpolation](feat) Add example of interpolation using np.convolve
    Signed-off-by: Krzysztof Zając <kzajac1997@gmail.com>

--- classes/interpolation/src/conv.py
+++ classes/interpolation/src/conv.py
@@ -2,9 +2,27 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-def conv1d_interpolate(x_measure: NDArray, y_measure: NDArray, x_interpolate: NDArray, kernel: callable) -> NDArray:
+def convolve_interpolate(
+    x_measure: NDArray, y_measure: NDArray, x_interpolate: NDArray, kernel: callable, return_kernels: bool = False
+) -> NDArray | tuple[NDArray, NDArray]:
+    width = x_measure[1] - x_measure[0]  # store period between samples
+    kernels = []
+
+    for x_sample, y_sample in zip(x_measure, y_measure):
+        # compute kernel for each sample using convolution, y_sample is just a single value
+        kernel = np.convolve(y_sample, kernel(x_interpolate, offset=x_sample, width=width), mode="same")
+        kernels.append(kernel)
+
+    y_interp = np.sum(kernels, axis=0)
+    if return_kernels:
+        return y_interp, kernels
+
+    return y_interp
+
+
+def product_interpolate(x_measure: NDArray, y_measure: NDArray, x_interpolate: NDArray, kernel: callable) -> NDArray:
     """
-    Interpolate using a convolution kernel
+    Interpolate using a convolution kernel and efficient dot product
 
     :param x_measure: x values of the measurements
     :param y_measure: y values of the measurements
@@ -26,7 +44,7 @@ def image_interpolate(image: NDArray, kernel: callable, ratio: int) -> NDArray:
         """Interpolate a single row or column of the image"""
         x_measure = np.arange(len(row))
         x_interpolate = np.linspace(0, len(row), ratio * len(row), endpoint=False)
-        return conv1d_interpolate(x_measure, row, x_interpolate, kernel)
+        return product_interpolate(x_measure, row, x_interpolate, kernel)
 
     interpolated = np.apply_along_axis(row_column_interpolate, 1, image)
     return np.apply_along_axis(row_column_interpolate, 0, interpolated)
