2f3e6720fee6d3028e26a83cfd611f6d061f28ed
Krzysztof Zając <kzajac1997@gmail.com>
2024-12-22 13:12:21 +0100

    [classes/conv](feat) Add _full_conv2d implementation
    Signed-off-by: Krzysztof Zając <kzajac1997@gmail.com>

--- classes/core/conv.py
+++ classes/core/conv.py
@@ -1,3 +1,4 @@
+# flake8: noqa: F203
 from typing import Literal
 
 import numpy as np
@@ -31,7 +32,7 @@ def _full_conv1d(signal: NDArray, kernel: NDArray, step: int = 1, padding: int =
                 # increment output position by summing aligned signal and kernel
                 output[conv_step] += signal[index] * kernel[kernel_step]
 
-    return np.asarray(output)
+    return output
 
 
 def _valid_conv1d(signal: NDArray, kernel: NDArray, step: int = 1, padding: int = 0) -> NDArray:
@@ -56,7 +57,44 @@ def _valid_conv1d(signal: NDArray, kernel: NDArray, step: int = 1, padding: int 
 
 
 def _full_conv2d(image: NDArray, kernel: NDArray, step: int = 1, padding: int = 0) -> NDArray:
-    raise NotImplementedError("2D convolution is not implemented yet!")
+    conv_input = np.pad(image, padding)
+    # reverse the kernel in both axes -> needed for simplified implementation to agree with mathematical definition
+    kernel = np.flipud(np.fliplr(kernel))
+
+    height, width = conv_input.shape
+    kernel_height, kernel_width = kernel.shape
+    # compute aligned length of output image, where x is either height or width of the image
+    # ((x + p) + m - 1) / s  (m = len(kernel), p = padding, s = step)
+    output_height = height + kernel_height - 1
+    output_width = width + kernel_width - 1
+    output_height = int(np.ceil(output_height / step))
+    output_width = int(np.ceil(output_width / step))
+
+    output = np.zeros((output_height, output_width))
+    # account for conv-steps, where kernel is not fully overlapping with the image with zero-padding
+    # the padding created with `padding` is included as "real" image data here
+    # the second zero-padding is used to avoid tracking negative indices in the convolution loop
+    conv_input = np.pad(image, ((kernel_height - 1, kernel_height - 1), (kernel_width - 1, kernel_width - 1)))
+
+    # h,w = tracking index for wight and height (using step = 1, equal to ((x + p) + m - 1) / s
+    # y = output, x = signal (or image), k = kernel, kh, kw = kernel.shape (kernel size)
+
+    # y[0, 0] = x[0,0] k[0,0]
+    # y[0, 1] = x[0,0] k[0,1] + x[0,1] k[0,0]
+    # ...
+    # y[0, w] = x[0,0] k[0,w] + x[0,1] k[0,w-1] + ... + x[0,w] k[0,0]
+    # ...
+    # y[h, w] = x[h,0] k[h,w] + x[h,1] k[h,w-1] + ... + x[h,w] k[h,0]
+    # ...
+    # y[h + kh, w + kw] = x[h,0] k[kh, kw] + x[h,1] k[kh, kw-1] + ... + x[h+kh, w+kw] k[0,0]
+
+    for conv_h_step in range(0, output_height, step):
+        for conv_w_step in range(0, output_width, step):
+            window = conv_input[conv_h_step : conv_h_step + kernel_height, conv_w_step : conv_w_step + kernel_width]
+            if window.shape == kernel.shape:  # kernel must fit into window
+                output[conv_h_step, conv_w_step] = np.sum(window * kernel)
+
+    return output
 
 
 def _valid_conv2d(image: NDArray, kernel: NDArray, step: int = 1, padding: int = 0) -> NDArray:
@@ -66,7 +104,7 @@ def _valid_conv2d(image: NDArray, kernel: NDArray, step: int = 1, padding: int =
 
     height, width = padded.shape
     kernel_height, kernel_width = kernel.shape
-    # compute aligned length of output signal, where x is either height or width of the image
+    # compute aligned length of output image, where x is either height or width of the image
     # ((x + p) - m + 1) / s  (m = len(kernel), p = padding, s = step)
     output_height = height - kernel_height + 1
     output_width = width - kernel_width + 1
@@ -85,7 +123,7 @@ def _valid_conv2d(image: NDArray, kernel: NDArray, step: int = 1, padding: int =
 
             # x is the signal, k is the kernel, h is tracking index for height, w is tracking index for width
             # x[h+kh, w+kw] * k[0, 0] + x[h+kh, w+kw+1] * k[0, 1] + ... + x[h+kh, w+kw+kw] * k[0, kw]
-            window = image[h_index: h_index + kernel_height, w_index: w_index + kernel_width]
+            window = image[h_index : h_index + kernel_height, w_index : w_index + kernel_width]
             output[conv_h_step, conv_w_step] = np.sum(window * kernel)
 
     return output
@@ -117,8 +155,11 @@ def convolve(
         case "valid", 1:
             return _valid_conv1d(signal, kernel, step, padding)
         case "full", 2:
-            ...
+            return _full_conv2d(signal, kernel, step, padding)
         case "valid", 2:
-            ...
+            return _valid_conv2d(signal, kernel, step, padding)
         case _, _:
-            raise ValueError(f"Unknown mode: {mode}")
+            if dim > 2:
+                raise ValueError(f"Only 1D and 2D signals are supported! {dim} > 2!")
+            else:
+                raise ValueError(f"Mode {mode} not supported!")
