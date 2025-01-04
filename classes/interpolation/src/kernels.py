bfaba0c552f1b313d1eb072b92a63610ccaa7694
Krzysztof Zając <kzajac1997@gmail.com>
2025-01-04 17:41:11 +0100

    [classes/interpolation] Update kernels to 2D interpolation
    Signed-off-by: Krzysztof Zając <kzajac1997@gmail.com>

--- classes/interpolation/src/kernels.py
+++ classes/interpolation/src/kernels.py
@@ -44,38 +44,39 @@ def keys_kernel(x: NDArray, offset: float, width: float, alpha: float = -0.5) ->
     ) * (x >= 1) * (x < 2) * (1 - (x >= 0) * (x < 1))
 
 
-def sample_hold_kernel2d(x: NDArray, offset: float, width: float) -> NDArray:
-    """Sample and hold interpolation kernel"""
-    x = x - offset
-    x, y = x[:, :, 0], x[:, :, 1]
+def sample_hold_kernel2d(xy: NDArray, offset: float | NDArray, width: float) -> NDArray:
+    """Sample and hold interpolation kernel. Offset can be single float or 2-element array"""
+    xy = xy - offset
+    x, y = xy[:, 0], xy[:, 1]
     return (x >= 0) * (x < width) * (y >= 0) * (y < width)
 
 
-def nearest_neighbour_kernel2d(x: NDArray, offset: float, width: float) -> NDArray:
-    """Nearest neighbour interpolation kernel"""
-    x = x - offset
-    x, y = x[:, :, 0], x[:, :, 1]
+def nearest_neighbour_kernel2d(xy: NDArray, offset: float, width: float) -> NDArray:
+    """Nearest neighbour interpolation kernel. Offset can be single float or 2-element array"""
+    xy = xy - offset
+    x, y = xy[:, 0], xy[:, 1]
     return (x >= (-1 * width / 2)) * (x < width / 2) * (y >= (-1 * width / 2)) * (y < width / 2)
 
 
-def linear_kernel2d(x: NDArray, offset: float, width: float) -> NDArray:
+def linear_kernel2d(xy: NDArray, offset: float, width: float) -> NDArray:
     """Nearest neighbour interpolation kernel"""
-    x = x - offset
-    x = x / width
-    x, y = x[:, :, 0], x[:, :, 1]
+    xy = xy - offset
+    xy = xy / width
+    x, y = xy[:, 0], xy[:, 1]
 
     return ((1 - np.abs(x)) * (1 - np.abs(y))) * (np.abs(x) < 1) * (np.abs(y) < 1)
 
 
-def sinc_kernel2d(x: NDArray, offset: float, width: float, alpha: float = np.inf) -> NDArray:
+def sinc_kernel2d(xy: NDArray, offset: float, width: float, alpha: float = np.inf) -> NDArray:
     """Normalized sine interpolation kernel"""
-    x = x - offset
-    x = x / width
-    x, y = x[:, :, 0], x[:, :, 1]
+    xy = xy - offset
+    xy = xy / width
+    x, y = xy[:, 0], xy[:, 1]
+
     return ((y >= -alpha) * (y < alpha) * (x >= -alpha) * (x < alpha)) * (np.sinc(x) * np.sinc(y))
 
 
-def keys_kernel2d(x: NDArray, offset: float, width: float, alpha: float = -0.5) -> NDArray:
+def keys_kernel2d(xy: NDArray, offset: float, width: float, alpha: float = -0.5) -> NDArray:
     """
     Interpolation kernel given by Keys bi-cubic function extended to 2D
 
@@ -87,9 +88,9 @@ def keys_kernel2d(x: NDArray, offset: float, width: float, alpha: float = -0.5) 
     def xy_range(xs: NDArray, ys: NDArray, xlow: float, xhigh: float, ylow: float, yhigh: float) -> NDArray:
         return (xs >= xlow) * (xs < xhigh) * (ys >= ylow) * (ys < yhigh)
 
-    x = x - offset
-    x = x / width
-    x, y = x[:, :, 0], x[:, :, 1]
+    xy = xy - offset
+    xy = xy / width
+    x, y = xy[:, 0], xy[:, 1]
     x = np.abs(x)
     y = np.abs(y)
 
